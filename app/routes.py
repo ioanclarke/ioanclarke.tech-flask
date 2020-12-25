53c08756c992d607e3f640fae6900a7804519398
ioanclarke <ioanclarke@gmail.com>
2020-12-25 23:59:10 +0000

    added index, projects, stats, base html files

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title='Projects')
+
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html', title='Stats')
