3eff3481646ae0d249abfeba6143e6e1efed8780
ioanclarke <ioanclarke@gmail.com>
2020-12-28 21:43:22 +0000

    added erros.py file

--- app/routes.py
+++ app/routes.py
@@ -42,7 +42,3 @@ def loading():
     return render_template('loading.html', title='...')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
